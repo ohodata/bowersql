20f4b3f53e81a3405ff14795ca04fe762ce32286
ohodata <stephen@ohodata.com>
2015-05-26 09:31:44 +0800

    Create DataNodeService.java

---
+++ src/com/bowerbird/rpc/DataNodeService.java
@@ -0,0 +1,30 @@
+package com.bowerbird.rpc;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.serialization.ObjectSerializationCodecFactory;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+public class DataNodeService {
+	private static final int PORT = 9123;
+
+    public static void main( String[] args ) throws IOException
+    {
+        IoAcceptor acceptor = new NioSocketAcceptor();
+
+        acceptor.getFilterChain().addLast( "logger", new LoggingFilter() );
+        acceptor.getFilterChain().addLast( "codec", new ProtocolCodecFilter( new ObjectSerializationCodecFactory()));
+
+        acceptor.setHandler( new DataNodeHandler() );
+        acceptor.getSessionConfig().setReadBufferSize( 2048 );
+        acceptor.getSessionConfig().setIdleTime( IdleStatus.BOTH_IDLE, 10 );
+        acceptor.bind( new InetSocketAddress(PORT) );
+    }
+}
